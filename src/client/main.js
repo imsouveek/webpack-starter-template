50436d1c2c3193977a0bca4b6a724cdfa7bd020e
Bose <Souveek_Bose@dell.com>
2019-09-23 14:17:01 +0530

    Initial Commit

--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,6 @@
 require('webpack-hot-middleware/client?reload=true');
 require('./main.css');
-require('./index.html');
+//require('./index.ejs');
 
 var a = async (args) => {
   const {a, b} = args;
