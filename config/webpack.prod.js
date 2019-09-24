a96d06aaf4b176f2146b3c56455abd2d34014dc1
Souveek Kumar Bose <souveek@Souveeks-MacBook-Pro.local>
2019-09-24 23:58:45 +0530

    TESTING NODE_ENV

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -58,11 +58,11 @@ module.exports = {
     },
   },
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
+    // new webpack.DefinePlugin({
+    //   "process.env": {
+    //     NODE_ENV: JSON.stringify("production")
+    //   }
+    // }),
     new OptimizeCssAssetsPlugin(),
     new MiniCssExtractPlugin({
       filename: "[name]-[contenthash].css"
